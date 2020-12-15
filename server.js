85d167ecbeaf6d19cf17364479c099da8935b06a
Victor Antonio <viictoorantonio@gmail.com>
2020-12-14 22:45:21 -0300

    Projeto inicial

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+let message = []
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+    if(message.length >= 1){
+        socket.emit('receptiveAllMessage', message)
+    }
+
+    socket.on('sendMessage', data => {
+        message.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+    })
+})
+
+server.listen(80)
